d3655ca6924b4356e9bfe8ab20ec002e4b17c2b8
AamnaZahid <AamnaZahid151@gmail.com>
2023-12-24 11:12:23 +0500

    Update UserService to handle user authentication

--- src/main/java/com/message/app/controller/UserController.java
+++ src/main/java/com/message/app/controller/UserController.java
@@ -8,8 +8,6 @@ import com.message.app.util.Constant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
