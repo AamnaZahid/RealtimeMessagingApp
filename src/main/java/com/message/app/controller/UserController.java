ac2bf128c36f361432b3daa57684164054e7c6ef
AamnaZahid <AamnaZahid151@gmail.com>
2023-12-26 02:50:33 +0500

    Update UserService to handle user authentication

--- src/main/java/com/message/app/controller/UserController.java
+++ src/main/java/com/message/app/controller/UserController.java
@@ -8,9 +8,13 @@ import com.message.app.util.Constant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @RestController
 public class UserController {
@@ -20,25 +24,28 @@ public class UserController {
     private UserService userService;
 
     @PostMapping(value = Constant.ADD_PATH)
-    public MessageDto registerUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<MessageDto> registerUser(@RequestBody UserDto userDto) {
         logger.info(userDto.toString());
         return userService.register(userDto);
     }
 
     @PostMapping(value = Constant.LOGIN_PATH)
-    public MessageDto loginUser(@RequestBody UserLoginDto loginDto) {
+    public ResponseEntity<MessageDto> loginUser(@RequestBody UserLoginDto loginDto) {
         MessageDto messageDto = new MessageDto();
         try {
-            messageDto = userService.login(loginDto);
-            if (Constant.SUCCESS.equals(messageDto.getResponseMessage())) {
+            ResponseEntity<MessageDto> loginStatus = userService.login(loginDto);
+            logger.info("Login Status: {}", loginStatus);
+            if (Constant.SUCCESS.equals(Objects.requireNonNull(loginStatus.getBody()).getResponseMessage())) {
                 messageDto.setResponseMessage(Constant.LOGIN_SUCCESS);
+                return ResponseEntity.ok(messageDto);
             } else {
                 messageDto.setResponseMessage(Constant.FAILED);
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageDto);
             }
         } catch (Exception e) {
             logger.error("Error creating session: {}", e.getMessage());
             messageDto.setResponseMessage(Constant.LOGIN_ERROR);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(messageDto);
         }
-        return messageDto;
     }
 }
--- src/main/java/com/message/app/servies/UserService.java
+++ src/main/java/com/message/app/servies/UserService.java
@@ -9,6 +9,8 @@ import com.message.app.repository.UserRepository;
 import com.message.app.util.Constant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.session.*;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
@@ -32,12 +34,14 @@ public class UserService {
     private RedisListService redisListService;
     private static final Logger logger = (Logger) LoggerFactory.getLogger(UserService.class);
 
-    public MessageDto register(UserDto userDto) {
+    public ResponseEntity<MessageDto> register(UserDto userDto) {
+        ResponseEntity<MessageDto> messageDtoResponseEntity = null;
         MessageDto messageDto = new MessageDto();
         try {
             if (userDto.getEmail() != null && userDto.getUserName() != null && userDto.getPassword() != null) {
                 if (userRepository.existsByEmail(userDto.getEmail())) {
                     messageDto.setResponseMessage(Constant.ALREADY_EXIST);
+                    messageDtoResponseEntity = ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageDto);
                 } else {
                     UserDomain userDomain = new UserDomain();
                     String password = securityConfig.encode(userDto.getPassword());
@@ -46,33 +50,41 @@ public class UserService {
                     userDomain.setPassword(password);
                     userRepository.save(userDomain);
                     messageDto.setResponseMessage(Constant.REGISTERED);
+                    messageDtoResponseEntity = ResponseEntity.status(HttpStatus.OK).body(messageDto);
                 }
             } else {
                 messageDto.setResponseMessage(Constant.FAILED);
+                messageDtoResponseEntity = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageDto);
             }
         } catch (Exception e) {
             logger.error("Error creating session: {}", e.getMessage());
             messageDto.setResponseMessage(Constant.FAILED);
+            messageDtoResponseEntity = ResponseEntity.internalServerError().body(messageDto);
         }
-        return messageDto;
+        return messageDtoResponseEntity;
     }
-
-    public MessageDto login(UserLoginDto loginDto) {
-        com.message.app.domain.UserDomain userDomain = userRepository.findByEmail(loginDto.getEmail());
+    public ResponseEntity<MessageDto> login(UserLoginDto loginDto) {
+        UserDomain userDomain = userRepository.findByEmail(loginDto.getEmail());
+        ResponseEntity<MessageDto> messageDtoResponseEntity = null;
         MessageDto messageDto = new MessageDto();
-        if (userDomain != null && securityConfig.passwordEncoder().matches(loginDto.getPassword(), userDomain.getPassword())) {
-            try {
+        try {
+            if (userDomain != null && securityConfig.passwordEncoder().matches(loginDto.getPassword(), userDomain.getPassword())) {
                 Session session = sessionRepository.createSession();
                 String sessionData = "User: " + loginDto.getEmail();
                 redisListService.pushDataToList("user:sessions:" + session.getId(), sessionData);
                 logger.info("Session created and data pushed to Redis for user: {}", loginDto.getEmail());
                 logger.info("Session ID: " + session.getId());
                 messageDto.setResponseMessage(Constant.SUCCESS);
-            } catch (Exception e) {
-                logger.error("Error creating session: {}", e.getMessage());
+                messageDtoResponseEntity = ResponseEntity.ok(messageDto);
+            } else {
                 messageDto.setResponseMessage(Constant.FAILED);
+                messageDtoResponseEntity = ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageDto);
             }
+        } catch (Exception e) {
+            logger.error("Error creating session: {}", e.getMessage());
+            messageDto.setResponseMessage(Constant.FAILED);
+            messageDtoResponseEntity = ResponseEntity.internalServerError().body(messageDto);
         }
-        return messageDto;
+        return messageDtoResponseEntity;
     }
 }
